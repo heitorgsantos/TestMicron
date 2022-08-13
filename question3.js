9b92803c4efd3d464b95828fa79439f0f55ff780
Heitor Santos <heitorg33@gmail.com>
2022-08-13 12:01:14 -0300

    Requisito 3

---
+++ question3.js
@@ -0,0 +1,6 @@
+const hexToRgb = hex =>
+  hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
+             ,(m, r, g, b) => '#' + r + r + g + g + b + b)
+    .substring(1).match(/.{2}/g)
+    .map(x => parseInt(x, 16))
+console.log(hexToRgb("#11AB24")) 
